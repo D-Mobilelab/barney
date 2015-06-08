d950596525311856d6695f42d14b218b14272c50
Roberto Bon <roberto.bon@buongiorno.com>
2015-06-08 17:27:26 +0200

    Config module and mock

--- mock/app.js
+++ mock/app.js
@@ -2,16 +2,19 @@
 
 angular.module('mock', [
     'ngRoute',
-    'barney.logger'
+    'barney.logger',
+    'barney.config'
 ])
-
 .config(['$routeProvider',
 	function ($routeProvider) {
-
 		$routeProvider
 			.when('/logger', {
 				templateUrl: 'pages/logger.html',
 				controller: 'LoggerCtrl'
+			})
+            .when('/config', {
+				templateUrl: 'pages/config.html',
+				controller: 'ConfigCtrl'
 			})
 			.otherwise({
 				redirectTo: '/'
