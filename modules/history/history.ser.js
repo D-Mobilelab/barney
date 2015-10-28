d93120260fdb8c8e08f522bea8422c4b4c74b60f
Aurelio <aurelio.merenda@buongiorno.com>
2015-10-28 16:06:39 +0100

    History module

---
+++ modules/history/history.ser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('barney.history').factory('BarneyHistory',
+	['$location', '$rootScope',
+	function($location, $rootScope){
+		
+		return {
+
+			previousPath: null,
+			
+			init: function(){
+				var _this = this;
+				$rootScope.$on('$locationChangeSuccess', function(event, newurl, oldurl){
+					_this.previousPath = oldurl.substr(oldurl.indexOf("#!")+2);
+				});
+			},
+
+			get: function(){
+				var _this = this;
+				return _this.previousPath;
+			},
+
+			goBack: function(){
+				var _this = this;
+				$location.url(_this.previousPath);
+			}
+
+		};
+
+	}
+]);
