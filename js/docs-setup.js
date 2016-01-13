c8a6953bb1f09d4bf256e5a8cb6bf726b3a86844
Aurelio <aurelio.merenda@buongiorno.com>
2016-01-13 18:08:32 +0100

    Integrated ngdoc to gruntfile as 'grunt doc' task

---
+++ js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "rfx.directive:rAutogrow",
+      "shortName": "rAutogrow",
+      "type": "directive",
+      "moduleName": "rfx",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically class content directive getcomputedstyle html input-block-level module ng-model pollyfill r-autogrow resize rfx size text textarea window"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
