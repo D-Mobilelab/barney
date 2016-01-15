6e0deb89951c0ef73b374eb21b94408365a48e44
Lorenzo Mussi <lorenzo.mussi@buongiorno.com>
2016-01-15 12:57:12 +0100

    BarneyDict docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -10,7 +10,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "barney.analytics",
       "shortDescription": "Analytics is the module of Barney for Google Analytics.",
-      "keywords": "analytics angular api app application barney custom dimension event file google import include js logger module overview session set track user web yourapplicationmodule"
+      "keywords": "analytics angular api app application barney custom dimension event file google import include js module overview session set track user web yourapplicationmodule"
     },
     {
       "section": "api",
@@ -20,6 +20,42 @@ NG_DOCS={
       "moduleName": "barney.analytics",
       "shortDescription": "Track page and event, set custom dimensions and set id",
       "keywords": "$scope $window action add analytics angular api app application assigned attributes barney barneyanalytics barneylogger category code component console controller custom define defined dependency dimension dimensions don easily enable enabled event example function future google hit homepagecontroller included init initialization label list logged logger logging menu method methods mock mode module note number object open options pageview pair pass phase renamed save saves service set setdimension setid slot title track trackevent tracking trackpage true ui user userstatus valuable verbose window"
+    },
+    {
+      "section": "api",
+      "id": "barney.dict",
+      "shortName": "barney.dict",
+      "type": "overview",
+      "moduleName": "barney.dict",
+      "shortDescription": "Dict is the module of Barney to show dictionary keys.",
+      "keywords": "angular api app application barney dict dictionary directive file filter import include js module overview provider web yourapplicationmodule"
+    },
+    {
+      "section": "api",
+      "id": "barney.dict.BarneyDict",
+      "shortName": "barney.dict.BarneyDict",
+      "type": "object",
+      "moduleName": "barney.dict",
+      "shortDescription": "Use dictionary service",
+      "keywords": "$scope $window add angular api attributes barney barneydict code component console controller default dependency dict dictionary easily example function homepagecontroller included init key list log missing mock module note object options prints provider renamed service showkey valued valued_key"
+    },
+    {
+      "section": "api",
+      "id": "barney.dict.directive:BarneyDict",
+      "shortName": "BarneyDict",
+      "type": "directive",
+      "moduleName": "barney.dict",
+      "shortDescription": "To use BarneyDict as a directive you can use it in your template files (html).",
+      "keywords": "api barney barneydict case code dict dictionary directive files html key print prints template valued_key"
+    },
+    {
+      "section": "api",
+      "id": "barney.dict.filter:BarneyDict",
+      "shortName": "BarneyDict",
+      "type": "filter",
+      "moduleName": "barney.dict",
+      "shortDescription": "BarneyDict as a filter is useful if:",
+      "keywords": "api attribute barney barneydict case code dict dictionary doesn examples filter html key print tag template text type valued_key"
     }
   ],
   "apis": {
