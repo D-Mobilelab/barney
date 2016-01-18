1261a764bd578867bd229be082914f44d95f0095
Aurelio <aurelio.merenda@buongiorno.com>
2016-01-18 17:10:11 +0100

    Dict and Meta test

--- test/modules/callbacky.test.js
+++ test/modules/callbacky.test.js
@@ -2,125 +2,175 @@
 
 describe('CALLBACKY -', function () {
 
-	// load the controller's module
+	var CallbackyService;	
+
 	beforeEach(module('barney.callbacky'));
 
-	var CallbackyService;
-	var fakeMethods, newValue, newValueInfo, logger;
-
 	beforeEach(inject(function (_BarneyCallbacky_) {
-		// define fakeMethods methods
-		fakeMethods = {
-			alert: function(value) {
-				newValue = value;
-			},
-			info: function(value) {
-				newValueInfo = value;
-			}
-		};	
-
-		// define logger methods
-		logger = {
-			log: function(){},
-			info: function(){},
-			warn: function(){},
-			error: function(){}
-		};
-
-		// spy logger.log
-		spyOn(logger, 'log');
-
-		// get and init CallbackyService
 		CallbackyService = _BarneyCallbacky_;
-		CallbackyService.init({
-			verbose: true,
-			logger: logger
-		});
 	}));
 
-	it('if bind a function with a key and trigger the same key, then that function is called', function(){
-		spyOn(fakeMethods, 'alert');
-		CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
-		CallbackyService.trigger('methodOne');
-		expect(fakeMethods.alert).toHaveBeenCalled();
+	describe('main -', function () {
+		var newValue, newValueInfo, fakeMethods;
+
+		beforeEach(function() {
+			fakeMethods = {
+				alert: function(value) {
+					newValue = value;
+				},
+				info: function(value) {
+					newValueInfo = value;
+				}
+			};
+
+			CallbackyService.init();
+		});
+
+		it('if bind a function with a key and trigger the same key, then that function is called', function(){
+			spyOn(fakeMethods, 'alert');
+			CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
+			CallbackyService.trigger('methodOne');
+			expect(fakeMethods.alert).toHaveBeenCalled();
+		});
+
+		it('if bind a function with a key and trigger the same key, then that function is called (DEPTH TEST)', function(){
+			CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
+			CallbackyService.trigger('methodOne');
+			expect(newValue).toEqual('hello');
+		});
+
+		it('if bind a function with a key and trigger the same key with args, then that function is called with args', function(){
+			spyOn(fakeMethods, 'alert');
+			CallbackyService.bind('methodOne', function(val){ fakeMethods.alert(val) });
+			CallbackyService.trigger('methodOne', 'world');
+			expect(fakeMethods.alert).toHaveBeenCalledWith('world');
+		});
+
+		it('if bind a function with a key and trigger the same key with args, then that function is called with args (DEPTH TEST)', function(){
+			CallbackyService.bind('methodOne', function(val){ fakeMethods.alert(val) });
+			CallbackyService.trigger('methodOne', 'world');
+			expect(newValue).toEqual('world');
+		});
+
+		it('if bind many function with a key and trigger the same key, then these functions are called', function(){
+			spyOn(fakeMethods, 'alert');
+			spyOn(fakeMethods, 'info');
+			CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
+			CallbackyService.bind('methodOne', function(){ fakeMethods.info('hello') });
+			CallbackyService.trigger('methodOne');
+			expect(fakeMethods.alert).toHaveBeenCalled();
+			expect(fakeMethods.info).toHaveBeenCalled();
+		});
+
+		it('if bind many function with a key and trigger the same key with args, then these functions are called with args', function(){
+			spyOn(fakeMethods, 'alert');
+			spyOn(fakeMethods, 'info');
+			CallbackyService.bind('methodOne', function(val){ fakeMethods.alert(val) });
+			CallbackyService.bind('methodOne', function(val){ fakeMethods.info(val) });
+			CallbackyService.trigger('methodOne', 'hello');
+			CallbackyService.trigger('methodOne', 'world');
+			expect(fakeMethods.alert).toHaveBeenCalledWith('hello');
+			expect(fakeMethods.alert).toHaveBeenCalledWith('world');
+		});
+
+		it('if bind a function with a key, clean that key and trigger the same key, then that functions is not called', function(){
+			spyOn(fakeMethods, 'alert');
+			CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
+			CallbackyService.clean('methodOne');
+			CallbackyService.trigger('methodOne');
+			expect(fakeMethods.alert.calls.count()).toEqual(0);
+		});
 	});
 
-	it('if bind a function with a key and trigger the same key, then that function is called (DEPTH TEST)', function(){
-		CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
-		CallbackyService.trigger('methodOne');
-		expect(newValue).toEqual('hello');
-	});
+	describe('logger -', function () {
+		var logger;
 
-	it('if bind a function with a key and trigger the same key with args, then that function is called with args', function(){
-		spyOn(fakeMethods, 'alert');
-		CallbackyService.bind('methodOne', function(val){ fakeMethods.alert(val) });
-		CallbackyService.trigger('methodOne', 'world');
-		expect(fakeMethods.alert).toHaveBeenCalledWith('world');
-	});
+		beforeEach(function() {
+			logger = {
+				log: function(){},
+				info: function(){},
+				warn: function(){},
+				error: function(){}
+			};
 
-	it('if bind a function with a key and trigger the same key with args, then that function is called with args (DEPTH TEST)', function(){
-		CallbackyService.bind('methodOne', function(val){ fakeMethods.alert(val) });
-		CallbackyService.trigger('methodOne', 'world');
-		expect(newValue).toEqual('world');
-	});
+			spyOn(logger, 'log');
+		});
 
-	it('if bind many function with a key and trigger the same key, then these functions are called', function(){
-		spyOn(fakeMethods, 'alert');
-		spyOn(fakeMethods, 'info');
-		CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
-		CallbackyService.bind('methodOne', function(){ fakeMethods.info('hello') });
-		CallbackyService.trigger('methodOne');
-		expect(fakeMethods.alert).toHaveBeenCalled();
-		expect(fakeMethods.info).toHaveBeenCalled();
-	});
+		describe('if verbose is true, ', function(){
+			beforeEach(function() {
+				CallbackyService.init({
+					verbose: true,
+					logger: logger
+				});
+			});
 
-	it('if bind many function with a key and trigger the same key with args, then these functions are called with args', function(){
-		spyOn(fakeMethods, 'alert');
-		spyOn(fakeMethods, 'info');
-		CallbackyService.bind('methodOne', function(val){ fakeMethods.alert(val) });
-		CallbackyService.bind('methodOne', function(val){ fakeMethods.info(val) });
-		CallbackyService.trigger('methodOne', 'hello');
-		CallbackyService.trigger('methodOne', 'world');
-		expect(fakeMethods.alert).toHaveBeenCalledWith('hello');
-		expect(fakeMethods.alert).toHaveBeenCalledWith('world');
-	});
+			it('then init method logs', function(){
+				expect(logger.log.calls.count()).toEqual(1);
+			});
 
-	it('if bind a function with a key, clean that key and trigger the same key, then that functions is not called', function(){
-		spyOn(fakeMethods, 'alert');
-		CallbackyService.bind('methodOne', function(){ fakeMethods.alert('hello') });
-		CallbackyService.clean('methodOne');
-		CallbackyService.trigger('methodOne');
-		expect(fakeMethods.alert.calls.count()).toEqual(0);
-	});
+			it('then bind method logs key and method', function(){
+				var key = 'methodOne';
+				var method = function(){ var a = 3; };
+				CallbackyService.bind(key, method);
+				CallbackyService.trigger(key);
+				expect(logger.log).toHaveBeenCalledWith('BarneyCallbacky', 'bind', key, method);
+			});
 
-	it('if verbose is true, then init method logs', function(){
-		expect(logger.log.calls.count()).toEqual(1);
-	});
+			it('then trigger method logs key and args', function(){
+				var key = 'methodOne';
+				var method = function(val){ var a = val; };
+				var arg = { hello: 'world' };
+				CallbackyService.bind(key, method);
+				CallbackyService.trigger(key, arg);
+				expect(logger.log).toHaveBeenCalledWith('BarneyCallbacky', 'trigger', key, arg);
+			});
 
-	it('if verbose is true, then bind method logs key and method', function(){
-		var key = 'methodOne';
-		var method = function(){ fakeMethods.alert('hello') };
-		CallbackyService.bind(key, method);
-		CallbackyService.trigger(key);
-		expect(logger.log).toHaveBeenCalledWith('BarneyCallbacky', 'bind', key, method);
-	});
+			it('then clean method logs key', function(){
+				var key = 'methodOne';
+				var method = function(){ var a = 3; };
+				CallbackyService.bind(key, method);
+				CallbackyService.clean(key);
+				expect(logger.log).toHaveBeenCalledWith('BarneyCallbacky', 'clean', key);
+			});
+		});
 
-	it('if verbose is true, then trigger method logs key and args', function(){
-		var key = 'methodOne';
-		var method = function(val){ fakeMethods.alert(val) };
-		var arg = { hello: 'world' };
-		CallbackyService.bind(key, method);
-		CallbackyService.trigger(key, arg);
-		expect(logger.log).toHaveBeenCalledWith('BarneyCallbacky', 'trigger', key, arg);
-	});
+		describe('if verbose is false,', function(){
+			beforeEach(function() {
+				CallbackyService.init({
+					verbose: false
+				});
+			});
 
-	it('if verbose is true, then clean method logs key', function(){
-		var key = 'methodOne';
-		var method = function(){ fakeMethods.alert('hello') };
-		CallbackyService.bind(key, method);
-		CallbackyService.clean(key);
-		expect(logger.log).toHaveBeenCalledWith('BarneyCallbacky', 'clean', key);
-	});
+			it('then init method does not logs', function(){
+				expect(logger.log.calls.count()).toEqual(0);
+			});
 
+			it('then bind method does not logs', function(){
+				var key = 'methodOne';
+				var method = function(){ var a = 3; };
+				CallbackyService.bind(key, method);
+				CallbackyService.trigger(key);
+				expect(logger.log.calls.count()).toEqual(0);
+			});
+
+			it('then trigger method does not logs', function(){
+				var key = 'methodOne';
+				var method = function(val){ var a = val; };
+				var arg = { hello: 'world' };
+				CallbackyService.bind(key, method);
+				CallbackyService.trigger(key, arg);
+				expect(logger.log.calls.count()).toEqual(0);
+			});
+
+			it('then clean method does not logs', function(){
+				var key = 'methodOne';
+				var method = function(){ var a = 3; };
+				CallbackyService.bind(key, method);
+				CallbackyService.clean(key);
+				expect(logger.log.calls.count()).toEqual(0);
+			});
+		});
+		
+	});
 
 });
---
+++ test/modules/dict.test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+describe('DICT -', function () {
+
+	var DictProvider;
+
+	beforeEach(module('barney.dict'));
+
+	beforeEach(inject(function (_BarneyDict_) {
+		DictProvider = _BarneyDict_;
+	}));
+
+	describe('default -', function () {
+		beforeEach(function() {
+			DictProvider.init();
+		});
+
+		it('if get a existent key, then it returns key value', function(){
+			expect(DictProvider.get('VALUED_KEY')).toEqual(DICTIONARY.VALUED_KEY);
+		});
+
+		it('if get a inexistent key, then it returns void string', function(){
+			expect(DictProvider.get('NULL_KEY')).toEqual('');
+		});
+	});
+
+	describe('all -', function () {
+		beforeEach(function() {
+			DictProvider.init({
+				showKey: 'all'
+			});
+		});
+
+		it('if get a existent key, then it returns key name enclosed in square brackets', function(){
+			expect(DictProvider.get('VALUED_KEY')).toEqual('[[VALUED_KEY]]');
+		});
+
+		it('if get a inexistent key, then it returns key name enclosed in square brackets', function(){
+			expect(DictProvider.get('NULL_KEY')).toEqual('[[NULL_KEY]]');
+		});
+	});
+
+	describe('missing -', function () {
+		beforeEach(function() {
+			DictProvider.init({
+				showKey: 'missing'
+			});
+		});
+
+		it('if get a existent key, then it returns key value', function(){
+			expect(DictProvider.get('VALUED_KEY')).toEqual(DICTIONARY.VALUED_KEY);
+		});
+
+		it('if get a inexistent key, then it returns key name enclosed in square brackets', function(){
+			expect(DictProvider.get('NULL_KEY')).toEqual('[[NULL_KEY]]');
+		});
+	});
+
+});
