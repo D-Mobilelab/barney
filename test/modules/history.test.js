add5c7e67640113c32e3b9ad2d188d467c847bd6
Lorenzo Mussi <lorenzo.mussi@buongiorno.com>
2016-02-02 14:36:37 +0100

    history test completed

--- test/modules/history.test.js
+++ test/modules/history.test.js
@@ -8,20 +8,25 @@ describe('HISTORY -', function () {
 	var newUrl = '#!/newUrl/';
 	var oldUrl = '#!/oldUrl/';
 
+	var location;
+
 	beforeEach(function(){
 		module('barney.history');
 
-		inject(function (_$rootScope_, _BarneyHistory_) {
+		inject(function (_$rootScope_, _BarneyHistory_, _$location_) {
 			$rootScope = _$rootScope_;
 			HistoryService = _BarneyHistory_;
+			location = _$location_;
 		});
 
 		spyOn($rootScope, "$on").and.callFake(function(stringa, method) {
 			changePageEvent = function(){
-				console.log('changePageEvent', method);
-				method.call(null, newUrl, oldUrl);
+				method.call(null, null, newUrl, oldUrl);
 			}	    	
 	    });
+
+	     spyOn(location, 'url');
+
 	});
 
 
@@ -31,19 +36,26 @@ describe('HISTORY -', function () {
 			expect($rootScope.$on).toHaveBeenCalled();
 		});
 
-		xit('expect get() method return old url, after change page event', function(){
+		it('expect get() method return old url, after change page event', function(){
 			HistoryService.init();
 			changePageEvent();
-			expect(HistoryService.get()).toEqual(oldUrl);
+			expect(HistoryService.get()).toEqual('/oldUrl/');
 		});
 
-		xit('expect get() method return old url, after multiple change page events', function(){
+		it('expect get() method return old url, after multiple change page events', function(){
 			HistoryService.init();
 			changePageEvent();
-			expect(HistoryService.get()).toEqual(oldUrl);
-			// cambiare oldurl
+			oldUrl = newUrl;
+			newUrl = '#!/newUrl2/'
 			changePageEvent();
-			expect(HistoryService.get()).toEqual(oldUrl);
+			expect(HistoryService.get()).toEqual('/newUrl/');
+		});
+
+		it('should redirect to previus url', function(){
+			HistoryService.init();
+			changePageEvent();
+			HistoryService.goBack()
+			expect(location.url).toHaveBeenCalledWith('/newUrl/');
 		});
 	});
 
