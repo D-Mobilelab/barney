e1e5ce2b9cdf50b7886afcba385a34a0c22ddb76
Aurelio <aurelio.merenda@buongiorno.com>
2016-02-02 11:52:53 +0100

    History test changePageEvent

--- test/modules/history.test.js
+++ test/modules/history.test.js
@@ -4,6 +4,7 @@ describe('HISTORY -', function () {
 
 	var HistoryService;
 	var $rootScope;
+	var changePageEvent;
 	var newUrl = '#!/newUrl/';
 	var oldUrl = '#!/oldUrl/';
 
@@ -16,16 +17,34 @@ describe('HISTORY -', function () {
 		});
 
 		spyOn($rootScope, "$on").and.callFake(function(stringa, method) {
-	    	method.call(null, newUrl, oldUrl);
+			changePageEvent = function(){
+				console.log('changePageEvent', method);
+				method.call(null, newUrl, oldUrl);
+			}	    	
 	    });
 	});
 
 
-	//describe('Init method - ', function(){
-		it('it save the last visited page', function(){
+	describe('Init method - ', function(){
+		it('expect $rootScope.$on has been called', function(){
 			HistoryService.init();
 			expect($rootScope.$on).toHaveBeenCalled();
 		});
-	//});
+
+		xit('expect get() method return old url, after change page event', function(){
+			HistoryService.init();
+			changePageEvent();
+			expect(HistoryService.get()).toEqual(oldUrl);
+		});
+
+		xit('expect get() method return old url, after multiple change page events', function(){
+			HistoryService.init();
+			changePageEvent();
+			expect(HistoryService.get()).toEqual(oldUrl);
+			// cambiare oldurl
+			changePageEvent();
+			expect(HistoryService.get()).toEqual(oldUrl);
+		});
+	});
 
 });
