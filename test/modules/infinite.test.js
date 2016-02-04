65283549b0f296ce5dd9ab93c4d7e4cb06163d4e
Aurelio <aurelio.merenda@buongiorno.com>
2016-02-04 17:55:29 +0100

    Infinite test

--- test/modules/infinite.test.js
+++ test/modules/infinite.test.js
@@ -1,129 +1,125 @@
 
 'use strict';
 
-describe('INFINTE', function() {
-  var element, timeout, mockedWindow, mockedDocument, $compile, scope,
-  $exceptionHandler, $compileProvider;
+describe('INFINITE', function() {
+    var $rootScope, $scope, $compile, $timeout, element, directiveScope, compileDirective;
 
-  beforeEach(function(){
-    // mock $window and $location providers
-    angular.mock.module(function($provide) {
-      $provide.value('window', {
-        innerHeight: 0,
-        pageYOffset: 0
-      });
+    beforeEach(function(){
+        module('barney.infinite');     
 
-      $provide.value('document', {
+        inject(function(_$rootScope_, _$compile_, _$timeout_) {
+            $rootScope = _$rootScope_;
+            $scope = _$rootScope_;
+            $compile = _$compile_;
+            $timeout = _$timeout_;
 
-        documentElement : {
-          offsetHeight: 0,
-          clientHeight: 0,
-          scrollHeight: 0
-        },
-
-        body: {
-          scrollHeight: 0 ,
-          offsetHeight: 0
-        }
-
-      });
-
-      $provide.value('$element', [{offsetHeight:500}]);
+            compileDirective = function(template){
+                element = angular.element(template);
+                $compile(element)($scope);
+                $scope.$digest();
+                directiveScope = element.isolateScope();
+                $rootScope.$apply();
+                $scope.$apply();
+            }
+        });
 
 
     });
-    // get barney module
-    module('barney.infinite');     
 
-    inject(function(_$compile_, $rootScope,
-    _$timeout_, _$document_) {
-    $compile = _$compile_;
-    timeout = _$timeout_;
-    //mockedWindow = _$window_;
-    //mockedDocument = _$document_;
-    scope = $rootScope.$new();
-    element = angular.element(
-      '<div id="spyThis" infinite-scroll infinite-enable="flag" infinite-callback="callApi" >'+
-          '<p ng-repeat="item in data">' +
-            '{{item}}' +
-          '</p>'+
-        '</div>');
+    describe("callback function -", function(){
+        beforeEach(function(){
+            $scope.flag = true;
+            $scope.callApi = function(aftercallme){ aftercallme(); };
+            spyOn($scope, 'callApi').and.callThrough();
+            compileDirective('<div infinite-scroll infinite-enable="flag" infinite-callback="callApi"></div>');
+        })
+
+        it('callback has been called', function(){
+            expect($scope.callApi).toHaveBeenCalled();
+        });
+
+        it('enable is false after callback', function(){
+            expect(directiveScope.enable).toBe(false);
+        });
     })
-  });
 
-  //afterEach(function() {
+    
+
+
+
+    //afterEach(function() {
  //   dealoc(element);
-  //});
+    //});
 
-  /*xit('should test infinite directive html', function(){
+    /*xit('should test infinite directive html', function(){
+                $compile(element)(scope);
+                    scope.flag = true;
+                    scope.data = [];
+                    var start = 0, step = 10;
+
+                    scope.callApi =  function(aftercallme){
+                            for(var i=start; i < start+step; i++){
+                                    scope.data.push(i);
+                            }
+                            start += step;
+                            aftercallme();
+                            timeout.flush();
+                        };
+                 expect(element.find('p').length).toEqual(0);
+
+    });
+
+    it('should test infinite directive html with scroll', function(){
+                    scope.flag = true;
+                    scope.data = [];
+                    var start = 0, step = 10;
+
+                    scope.callApi =  function(aftercallme){
+
+                            for(var i=start; i < start+step; i++){
+                                    scope.data.push(i);
+                            }
+
+                            start += step;
+
+                            aftercallme();
+                        };
+                    $compile(element)(scope);
+                    element.isolateScope().enable = true;
+                    scope.$digest();
+                    expect(element.find('p').length).toBe(10);
+    });*/
+
+    xit('Should do enable remain false', function(){
+        scope.callApi =  function(aftercallme){
+        };
+        scope.flag = true;
+        scope.data = [];
         $compile(element)(scope);
-          scope.flag = true;
-          scope.data = [];
-          var start = 0, step = 10;
+        scope.$digest();
+        expect(element.isolateScope().enable).toBe(false);
+    });
 
-          scope.callApi =  function(aftercallme){
-              for(var i=start; i < start+step; i++){
-                  scope.data.push(i);
-              }
-              start += step;
-              aftercallme();
-              timeout.flush();
-            };
-         expect(element.find('p').length).toEqual(0);
+    xit('Should do enable be true', function(){
+        scope.flag = true;
+        scope.data = [];
+        var start=0, step=10;
 
-  });
+        scope.callApi =  function(aftercallme){
 
-  it('should test infinite directive html with scroll', function(){
-          scope.flag = true;
-          scope.data = [];
-          var start = 0, step = 10;
+            for(var i=start; i < start+step; i++){
+                    scope.data.push(i);
+            }
 
-          scope.callApi =  function(aftercallme){
+            start += step;
 
-              for(var i=start; i < start+step; i++){
-                  scope.data.push(i);
-              }
+            aftercallme();
+        };
+        $compile(element)(scope);
+        scope.$digest();
+        spyOn(element.isolateScope(), 'callback');
 
-              start += step;
-
-              aftercallme();
-            };
-          $compile(element)(scope);
-          element.isolateScope().enable = true;
-          scope.$digest();
-          expect(element.find('p').length).toBe(10);
-  });*/
-
-  it('Should do enable remain false', function(){
-    scope.callApi =  function(aftercallme){
-    };
-    scope.flag = true;
-    scope.data = [];
-    $compile(element)(scope);
-    scope.$digest();
-    expect(element.isolateScope().enable).toBe(false);
-  });
-
-  it('Should do enable be true', function(){
-    scope.flag = true;
-    scope.data = [];
-    var start=0, step=10;
-
-    scope.callApi =  function(aftercallme){
-
-      for(var i=start; i < start+step; i++){
-          scope.data.push(i);
-      }
-
-      start += step;
-
-      aftercallme();
-    };
-    $compile(element)(scope);
-    scope.$digest();
-    spyOn(element.isolateScope(), 'callback');
-
-    expect(element.isolateScope().callback).toHaveBeenCalled();
-  });
+        expect(element.isolateScope().callback).toHaveBeenCalled();
+    });
 
 });
