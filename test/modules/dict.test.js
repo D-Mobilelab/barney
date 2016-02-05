bfb8bc160f2b085074d461ac55363241436484ca
Lorenzo Mussi <lorenzo.mussi@buongiorno.com>
2016-02-05 09:51:06 +0100

    END TEST, 97.26% reached

--- test/modules/dict.test.js
+++ test/modules/dict.test.js
@@ -83,4 +83,76 @@ describe('DICT -', function () {
 			expect($filter('dict')('NULL_KEY')).toBe('[[NULL_KEY]]');
 		}));
 	});
+
+	describe('Dict Directive with default', function(){
+		var $rootScope, $scope, $compile, element, directiveScope, 
+			compileDirective;
+
+		beforeEach(function() {
+			DictProvider.init();
+
+			inject(function(_$rootScope_, _$compile_) {
+	            $rootScope = _$rootScope_;
+	            $scope = _$rootScope_;
+	            $compile = _$compile_;
+
+	            compileDirective = function(template){
+	                element = angular.element(template);
+	                $compile(element)($scope);
+	                $scope.$digest();
+	                directiveScope = element.isolateScope();
+	                $rootScope.$apply();
+	                $scope.$apply();
+	            }
+	        });
+		});
+
+		it('should contain Hello World', function(){
+			compileDirective('<dict key="VALUED_KEY"></dict>');
+			expect(element.html()).toContain('Hello world!');
+		});
+
+		it('should contain nothing', function(){
+			compileDirective('<dict key="NULL_KEY"></dict>');
+			expect(element.html()).toEqual('');
+		});
+	});	
+
+	describe('Dict Directive with all', function(){
+		var $rootScope, $scope, $compile, element, directiveScope, 
+			compileDirective;
+
+		beforeEach(function() {
+			DictProvider.init({
+				showKey: 'all'
+			});
+
+			inject(function(_$rootScope_, _$compile_) {
+	            $rootScope = _$rootScope_;
+	            $scope = _$rootScope_;
+	            $compile = _$compile_;
+
+	            compileDirective = function(template){
+	                element = angular.element(template);
+	                $compile(element)($scope);
+	                $scope.$digest();
+	                directiveScope = element.isolateScope();
+	                $rootScope.$apply();
+	                $scope.$apply();
+	            }
+	        });
+		});
+
+		it('should contain [[VALUED_KEY]]', function(){
+			compileDirective('<dict key="VALUED_KEY"></dict>');
+			expect(element.html()).toContain('[[VALUED_KEY]]');
+		});
+
+		it('should contain NULL_KEY', function(){
+			compileDirective('<dict key="NULL_KEY"></dict>');
+			expect(element.html()).toContain('[[NULL_KEY]]');
+		});
+	});	
+
+
 });
