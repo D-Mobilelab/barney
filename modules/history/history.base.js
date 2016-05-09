c868c0c27a85ea7a269b94db93098df0af1158d5
Aurelio <aurelio.merenda@buongiorno.com>
2016-05-09 17:43:17 +0200

    Module history+utility

---
+++ modules/history/history.base.js
@@ -0,0 +1,24 @@
+if(!barney){ barney = {} }
+barney.history = new function(){
+
+	var previousPath = null;
+    var previousState = null;
+
+    this.init = function(){
+    	var _this = this;
+    	window.addEventListener("hashchange", function(newurl, oldurl){
+    		if(newurl !== oldurl){
+                _this.previousPath = oldurl.substr(oldurl.indexOf('#!') + 2);
+            }
+    	}, false);
+    };
+
+    this.setPrevPath = function(path){
+    	previousPath = path;
+    };
+
+    this.getPrevPath = function(){
+        return previousPath;
+    };
+
+}
---
+++ modules/utility/utility.base.js
@@ -0,0 +1,18 @@
+var BarneyUtility = new function(){
+
+	this.arrayDiff = function(first, second){
+    	return first.filter(function(i) {
+            return second.indexOf(i) < 0;
+        });
+    };
+
+    this.mediaMatcher = function(mediaquery, callback){
+    	var isMatchMediaSupported = !!(window && window.matchMedia);
+        if(typeof mediaquery === 'string' && typeof callback === 'function' && isMatchMediaSupported ){
+            var mql = window.matchMedia(mediaquery);
+            callback(mql);
+            mql.addListener(callback); 
+        }
+    };
+
+}
