3484380727c64bc1703a9362f4b9028360a56bc3
Lorenzo Mussi <lorenzo.mussi@buongiorno.com>
2016-05-12 11:01:32 +0200

    newton docs

--- modules/newton/newton.js
+++ modules/newton/newton.js
@@ -1,3 +1,36 @@
+/**
+ * @ngdoc object
+ * @name newton
+ *
+ * @description
+ * Newton is the module for Newton.
+ *
+ * You can use it to:
+ * - track a page
+ * - track an event
+ * - track the timespent on full website or page
+ *
+ * # Import & Usage
+ * ## Vanilla JS
+ * Import this single file to use Newton module, in Vanilla JS:
+ * <pre>
+ * <script type="text/javascript" src="./bower_components/barney/base/newton.js"> 
+ * </pre>
+ * Now you can use global object **barney.Newton** and associated methods, described below. <br/>
+ * You can start by initializing it with {@link newton#methods_init init} method.
+ *
+ * ## Angular
+ * Import this single file to use Newton module, in Angular framework:
+ * <pre>
+ * <script type="text/javascript" src="./bower_components/barney/angular/newton.js"> 
+ * </pre>
+ * Now you can import **barney** module for your Angular app.
+ * <pre>
+ * angular.module('yourApp', [ 'barney' ]);
+ * </pre>
+ * Newton module for Angular contains only {@link newton.BarneyNewton BarneyNewton service}.
+ */
+
 barney.Newton = new function(){
 
     var enabled = true;
@@ -9,6 +42,25 @@ barney.Newton = new function(){
         warn: function(){},
         error: function(){}
     };
+
+
+    /**
+     * @ngdoc function
+     * @name newton#init
+     * @methodOf newton
+     *
+     * @description 
+     * Init newton service.
+     *
+     * - **enabled**: enable or disable the module (default: true)
+     * - **verbose**: logs all the works done by the module (default: false)
+     * - **logger**: object that logs (i.e: window.console, BarneyLogger, ...) (default: null)
+     *
+     * @param {Object} options list of options
+     * @param {boolean} [options.enabled=true] enable/disable tracking on Newton
+     * @param {boolean} [options.verbose=false] enable/disable verbose mode, with logging
+     * @param {Object} [options.logger={}] logging methods to use for verbose mode
+     */
 
     this.init = function(options){
         if(options){
@@ -28,6 +80,29 @@ barney.Newton = new function(){
         }
     };
 
+    /**
+     * @ngdoc function
+     * @name newton#trackPage
+     * @methodOf newton
+     *
+     * @description Track a page view event.
+     *
+     * @param {Object} options Page details (i.e: title page, URL, ...)
+     *
+    * @example
+     * # Newton trackPage 
+     * Here is an example of the trackPage method.
+     * <pre>
+     * 
+     *   barney.Newton.trackPage({
+     *      title: 'page_pitle',
+     *      URL: 'http://page_url'
+     *  });
+     *
+     * </pre>
+     * 
+    */
+
     this.trackPage = function(options){
         if(enabled){
             Newton.getSharedInstance().sendEvent('pageview', Newton.SimpleObject.fromJSONObject(options));
@@ -37,6 +112,28 @@ barney.Newton = new function(){
             logger.log('BarneyNewton', 'track', 'pageview', options);
         }
     };
+
+    /**
+     * @ngdoc function
+     * @name newton#trackEvent
+     * @methodOf newton
+     *
+     * @description Track an event.
+     *
+     * @param {Object} options Event details (i.e: ClickEvent, ShareEvent, ...)
+     *
+     * @example
+     * # Newton trackEvent 
+     * Here is an example of the trackEvent method.
+     * <pre>
+     * 
+     *   barney.Newton.trackEvent('Click', {
+     *      title: 'generated_event'
+     *  });
+     *
+     * </pre>
+     *
+    */
 
 
     this.trackEvent = function(eventName, options){
@@ -48,6 +145,30 @@ barney.Newton = new function(){
             logger.log('BarneyNewton', 'track', eventName, options);
         }
     };
+
+    /**
+     * @ngdoc function
+     * @name newton#startHeartbeat
+     * @methodOf newton
+     *
+     * @description Start a single Heartbeat.
+     *
+     * @param {string} keyword Heartbeat name
+     * @param {Object} options Heartbeat details
+     *
+     * @example
+     * # Newton startHeartbeat 
+     * Here is an example of the startHeartbeat method.
+     * <pre>
+     * 
+     *   barney.Newton.startHeartbeat('aTestHeart', {
+     *      label: 'page_title',
+     *      valuable: 'no',
+     *      action: 'no'
+     *   });
+     *
+     * </pre>
+    */
 
     this.startHeartbeat = function(keyword, options){
         if(!heartbeats[keyword]){
@@ -67,6 +188,19 @@ barney.Newton = new function(){
         }    
     };
 
+
+    /**
+     * @ngdoc function
+     * @name newton#stopHeartbeat
+     * @methodOf newton
+     *
+     * @description Stop a single Heartbeat.
+     *
+     * @param {string} keyword Heartbeat name
+     * @param {Object} options Heartbeat details (can be also an empty object)
+     *
+    */
+
     this.stopHeartbeat = function(keyword, options){
         if(heartbeats[keyword]){
             if(enabled){
@@ -83,6 +217,15 @@ barney.Newton = new function(){
         }
     };
 
+    /**
+     * @ngdoc function
+     * @name newton#stopAllHeartbeat
+     * @methodOf newton
+     *
+     * @description Stop all running heartbeats.
+     *
+    */
+
     this.stopAllHeartbeat = function(){
         if(enabled){
             for(var key in heartbeats){
@@ -95,9 +238,29 @@ barney.Newton = new function(){
         }
     };
 
+    /**
+     * @ngdoc function
+     * @name newton#heartbeatsList
+     * @methodOf newton
+     *
+     * @description Get the list of all running heartbeats.
+     *
+    */
+
     this.heartbeatsList = function(){
         return heartbeats;
     };
+
+   /**
+     * @ngdoc function
+     * @name newton#isHeartbeatEnabled
+     * @methodOf newton
+     *
+     * @description Check if an heartbeat is running or not, returning true or false.
+     *
+     * @param {string} keyword Heartbeat name
+     *
+    */
 
     this.isHeartbeatEnabled = function(keyword){
         return heartbeats[keyword];
--- modules/newton/newton.ser.js
+++ modules/newton/newton.ser.js
@@ -1,34 +1,109 @@
+/**
+ * @ngdoc object
+ * @name newton.BarneyNewton
+ *
+ * @description 
+ * Angular service of {@link newton Newton} module.
+ *
+ * # Import & Usage
+ * To use Newton service, you have to add BarneyNewton 
+ * dependency to your component (directive, controller...).
+ * <pre>
+ * angular.module('mock').controller('HomePageController', [
+ *     'BarneyNewton',
+ *     function(BarneyNewton){
+ *         // we can use "BarneyNewton" object here
+ *     }
+ * ]);
+ * </pre>
+ */
 
 angular.module('barney').factory('BarneyNewton', function(){
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#init
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_init init} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.init = function(options){
         barney.Newton.init(options);
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#trackPage
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_trackPage trackPage} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.trackPage = function(options){
         barney.Newton.trackPage(options);
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#trackEvent
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_trackEvent trackEvent} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.trackEvent = function(event, options){
         barney.Newton.trackEvent(event, options);
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#startHeartbeat
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_startHeartbeat startHeartbeat} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.startHeartbeat = function(keyword, params){
         barney.Newton.startHeartbeat(keyword, params);
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#stopHeartbeat
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_stopHeartbeat stopHeartbeat} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.stopHeartbeat = function(keyword){
         barney.Newton.stopHeartbeat(keyword);
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#stopAllHeartbeat
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_stopAllHeartbeat stopAllHeartbeat} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.stopAllHeartbeat = function(){
         barney.Newton.stopAllHeartbeat();
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#heartbeatsList
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_heartbeatsList heartbeatsList} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.heartbeatsList = function(){
         return barney.Newton.heartbeatsList();
     };
 
+    /**
+     * @ngdoc function
+     * @name newton.BarneyNewton#isHeartbeatEnabled
+     * @methodOf newton.BarneyNewton
+     *
+     * @description Refer to {@link newton#methods_isHeartbeatEnabled isHeartbeatEnabled} method of Newton module, replacing *barney.Newton* to *BarneyNewton*.
+     */
     this.isHeartbeatEnabled = function(keyword){
         return barney.Newton.getSingleHeartbeat(keyword);
     };
