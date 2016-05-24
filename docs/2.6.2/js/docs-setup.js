80e5e61daa926a4f600fc476ff03993a1173c724
Lorenzo Mussi <lorenzo.mussi@buongiorno.com>
2016-05-24 11:35:07 +0200

    live html patch

---
+++ docs/2.6.2/js/docs-setup.js
@@ -0,0 +1,259 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "analytics",
+      "shortName": "analytics",
+      "type": "overview",
+      "moduleName": "analytics",
+      "shortDescription": "Analytics",
+      "keywords": "analytics barney barneyanalytics components custom dimension event file google html include init initialize initialized javascript js method methods_init min module overview session set src text track type usage user version"
+    },
+    {
+      "section": "version",
+      "id": "analytics.BarneyAnalytics",
+      "shortName": "analytics.BarneyAnalytics",
+      "type": "object",
+      "moduleName": "analytics",
+      "shortDescription": "Track pageview and event, set custom dimensions and id",
+      "keywords": "$scope $window action add analytics angular app application assigned attributes barneyanalytics barneylogger category code component console controller custom define defined dependency dimension dimensions doesn easily enable enabled event example function future google hit homepagecontroller included init initialization label list logged logger logging menu method methods methods_init methods_setdimension methods_setid methods_trackevent methods_trackpage mock mode module note number object open options pageview pair pass phase renamed save saves service set setdimension setid slot title track trackevent tracking trackpage true ui user userstatus valuable verbose version window"
+    },
+    {
+      "section": "version",
+      "id": "callbacky",
+      "shortName": "callbacky",
+      "type": "overview",
+      "moduleName": "callbacky",
+      "shortDescription": "Callbacky",
+      "keywords": "allow barney barneycallbacky bind callbacky code components controllers files function html include init initialize initialized javascript js method methods_init min module overview points services src text trigger type usage version"
+    },
+    {
+      "section": "version",
+      "id": "callbacky.BarneyCallbacky",
+      "shortName": "callbacky.BarneyCallbacky",
+      "type": "object",
+      "moduleName": "callbacky",
+      "shortDescription": "To use Callbacky service, you have to add BarneyCallbacky dependency ",
+      "keywords": "$scope add angular arg arguments attributes barneycallbacky barneylogger bind bound call callbacky called clean code component console controller corresponding dependency directive easily event example file function hello homepagecontroller identify included init initialize instruction key list log logger logs method methods methods_bind methods_clean methods_init methods_trigger mock module note object options pass relative remove renamed requires saved second service set string trigger triggered unique values verbose version window work world"
+    },
+    {
+      "section": "version",
+      "id": "config",
+      "shortName": "config",
+      "type": "overview",
+      "moduleName": "config",
+      "shortDescription": "Config",
+      "keywords": "barney barneyconfig components config enable_login environment example false file filter html include init javascript js json key_false key_false_string key_null key_null_string key_true key_void_string key_zero key_zero_string method methods_init min module newton_secret_key null object order overview reads src stored text true type usage var variables version"
+    },
+    {
+      "section": "version",
+      "id": "config.BarneyConfig",
+      "shortName": "config.BarneyConfig",
+      "type": "object",
+      "moduleName": "config",
+      "shortDescription": "To use Config service, you have to add BarneyConfig dependency to your component (directive, controller...).",
+      "keywords": "$scope add angular barneyconfig barneyconifg code component config configuration conifg controller dependency easily enable_login example false function homepagecontroller included init initialize key key_false key_false_string key_null key_null_string key_true key_void_string key_zero key_zero_string list method methods methods_get methods_init methods_list mock module newton_secret_key note null object options renamed requires return service specific true values version"
+    },
+    {
+      "section": "version",
+      "id": "config.filter:config",
+      "shortName": "config",
+      "type": "filter",
+      "moduleName": "config",
+      "shortDescription": "You can get a config value also using a custom config filter",
+      "keywords": "config custom enable_login filter version"
+    },
+    {
+      "section": "version",
+      "id": "dict",
+      "shortName": "dict",
+      "type": "overview",
+      "moduleName": "dict",
+      "shortDescription": "Dict",
+      "keywords": "barney barneydict components dict dictionary directive file filter html include init initialize initialized javascript js method methods_init min module overview src text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "dict.BarneyDict",
+      "shortName": "dict.BarneyDict",
+      "type": "object",
+      "moduleName": "dict",
+      "shortDescription": "To use Dict provider, you have to add BarneyDict dependency to your component (directive, controller...).",
+      "keywords": "$scope $window add angular attributes barneydict code component console controller default dependency dict dictionary easily example existkey function hello homepagecontroller included init key list log method methods methods_get methods_init methods_list missing mock module note notexistkey object options prints provider renamed service showkey undefined valued valued_key version world"
+    },
+    {
+      "section": "version",
+      "id": "dict.directive:dict",
+      "shortName": "dict",
+      "type": "directive",
+      "moduleName": "dict",
+      "shortDescription": "To use BarneyDict as a directive you can use it in your template files (html).",
+      "keywords": "barneydict code dict dictionary directive files hello html key print prints template valued_key version"
+    },
+    {
+      "section": "version",
+      "id": "dict.filter:dict",
+      "shortName": "dict",
+      "type": "filter",
+      "moduleName": "dict",
+      "shortDescription": "With BarneyDict you can print the value of a dictionary key but it hasn&#39;t to contain html code.",
+      "keywords": "attribute barneydict case code dict dictionary directive filter hasn html key print tag template text type valued_key version"
+    },
+    {
+      "section": "version",
+      "id": "history",
+      "shortName": "history",
+      "type": "overview",
+      "moduleName": "history",
+      "shortDescription": "History",
+      "keywords": "barney barneyhistory browser components file history html include init initialize initialized javascript js method methods methods_init min module overview provide src text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "history.BarneyHistory",
+      "shortName": "history.BarneyHistory",
+      "type": "object",
+      "moduleName": "history",
+      "shortDescription": "Use History service",
+      "keywords": "$scope add angular barneyhistory called code component console controller dependency directive easily example function getprevpath getprevstate goback history homepagecontroller included init list log method methods methods_getprevpath methods_getprevstate methods_goback methods_init mock module note object path renamed saved service version visited"
+    },
+    {
+      "section": "version",
+      "id": "infinite",
+      "shortName": "infinite",
+      "type": "overview",
+      "moduleName": "infinite",
+      "shortDescription": "Infinite",
+      "keywords": "activate attribute barney bottom call calls care components contents controller directive disable documentation doesn don example file fill function html include infinite infinte inside javascript js listen listens min module offset overview pass processing reached reaching reactivate reenable remember scroll scrolled src text type usage version window worry"
+    },
+    {
+      "section": "version",
+      "id": "infinite.directive:InfiniteScroll",
+      "shortName": "InfiniteScroll",
+      "type": "directive",
+      "moduleName": "infinite",
+      "shortDescription": "Allow infinite scroll on your page.",
+      "keywords": "$scope activate add allow api boolean bottom call callapimethod called choose controller data define defined directive disabled div element example executed flag function functiontocall infinite infinite-callback infinite-enable infinite-offset infinite-scroll item list listener ng-repeat offset pass re-enable reached reaching reactivate reenable reenableinfinite remember response scroll true version wait work"
+    },
+    {
+      "section": "version",
+      "id": "logger",
+      "shortName": "logger",
+      "type": "overview",
+      "moduleName": "logger",
+      "shortDescription": "Logger",
+      "keywords": "advanced barney barneylogger barneyrotatinglogger baselogger components external files init initialize initialized javascript js levels log logger method methods_init min module ordered organized overview point relevance relevant remember rotatinglogger sorted src system text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "logger.BarneyLogger",
+      "shortName": "logger.BarneyLogger",
+      "type": "object",
+      "moduleName": "logger",
+      "shortDescription": "Use Logger service.",
+      "keywords": "$scope actives add addictional allows angular app args arguments attributes barneylogger call change code component config configuration console controller custom defines dependency directive disabled easily emit emitter enable enabled error errors example examples false function general getconfig getter hello homepagecontroller included info init initialization initialize isenabled js key level levels list log logger logs manual message messages method methods methods_error methods_getconfig methods_info methods_init methods_isenabled methods_log methods_table methods_warn mock module note object options parameter personalized read-only recommended remember renamed sends service site store table true version warn warning warnings window world"
+    },
+    {
+      "section": "version",
+      "id": "logger.BarneyRotatingLogger",
+      "shortName": "logger.BarneyRotatingLogger",
+      "type": "object",
+      "moduleName": "logger",
+      "shortDescription": "Use RotatingLogger service.",
+      "keywords": "$scope add adding angular api api_send app attribute attributes automatically barneylogger barneyrotatinglogger baselogger call calling calls change click code component configuration configure controller custom defines dependency directive disabled documentation doesn easily enable enabled enables endrecording error example export file function functionality getconfig homepagecontroller included info init initialization initialize internal isenabled js json length list log logged logger max maxsize message messages method methods methods_endrecording methods_error methods_getconfig methods_info methods_init methods_isenabled methods_log methods_startrecording methods_table methods_warn mock mode module note number object options original recommended record recorded recording recordingenabled records remember renamed requires rotating rotatinglogger save saverecords saves sends service set site sliding start startrecording starts table tells true version warn window work wraps"
+    },
+    {
+      "section": "version",
+      "id": "meta",
+      "shortName": "meta",
+      "type": "overview",
+      "moduleName": "meta",
+      "shortDescription": "Meta",
+      "keywords": "barney barneymeta components dinamically file fill html include init initialize initialized javascript js manage meta metatags method methods_init min module overview src tags text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "meta.BarneyMeta",
+      "shortName": "meta.BarneyMeta",
+      "type": "object",
+      "moduleName": "meta",
+      "shortDescription": "Meta is the module of Barney to fill meta tags dinamically.",
+      "keywords": "$on $rootscope $routechangestart $scope add angular app barney barneymeta code component console controller current default defaults dependency description dinamically easily event example fill function homepagecontroller image include included init inizialize jpg js key key2 list log manage meta metatags method methods methods_default methods_get methods_init methods_list methods_revert methods_set mock module note object pass passed passing previous print product recommended renamed restore revert routechangestart service set single standard standard-image tags title value1 value2 values version"
+    },
+    {
+      "section": "version",
+      "id": "newton",
+      "shortName": "newton",
+      "type": "overview",
+      "moduleName": "newton",
+      "shortDescription": "Newton",
+      "keywords": "allows barney barneynewton components events file html include init initialize initialized javascript js library method methods_init min module newton overview src text track type usage version"
+    },
+    {
+      "section": "version",
+      "id": "newton.BarneyNewton",
+      "shortName": "newton.BarneyNewton",
+      "type": "object",
+      "moduleName": "newton",
+      "shortDescription": "To use Newton service, you have to add BarneyNewton dependency to your component (i.e: directive, controller...).",
+      "keywords": "$scope action add addeventlistener angular atestheart attributes barneylogger barneynewton call callback called calling click closed code complete component console controller custom custom_data customlogin dependency details directive disable document easily enable enabled event events example false function generated generated_event getsingleheartbeat heartbeat heartbeats heartbeatslist homepagecontroller http identifier included indicates init initial initialize interval keyword label list log logged logger login logs mandatory message method methods methods_customlogin methods_getsingleheartbeat methods_heartbeatslist methods_init methods_startheartbeat methods_stopallheartbeat methods_stopheartbeat methods_trackevent methods_trackpage mock module msisdn newton note null object options page_pitle page_title parameters params properties recommended removed renamed requires retrieve running secret secretid send service single site spent started startheartbeat starting stopallheartbeat stopheartbeat stopping successful time title track trackevent tracking trackpage true unload unlogged url user userid userprops valuable verbose version visited window work works"
+    },
+    {
+      "section": "version",
+      "id": "storage",
+      "shortName": "storage",
+      "type": "overview",
+      "moduleName": "storage",
+      "shortDescription": "Storage",
+      "keywords": "barney barneystorage components cookies file html include init initialize initialized javascript js jsobject local method methods_init min module overview set src storage text type usage values version"
+    },
+    {
+      "section": "version",
+      "id": "storage.BarneyStorage",
+      "shortName": "storage.BarneyStorage",
+      "type": "object",
+      "moduleName": "storage",
+      "shortDescription": "To use Storage service, you have to add BarneyStorage dependency to your component (i.e: directive, controller...).",
+      "keywords": "$scope add alert angular array attributes barneylogger barneystorage browser called code component console controller cookie data default define defined delete deletes dependency directive easily example function getmultiple homepagecontroller identify included init initialize islocalstoragesupported jsobject key key1 key2 list local localstorage log logger method methods_delete methods_get methods_getmultiple methods_init methods_islocalstoragesupported methods_set methods_setmultiple mock module multiple note object options param params renamed save service set setmultiple sets storage store stored stores support supported type values version window"
+    },
+    {
+      "section": "version",
+      "id": "utility",
+      "shortName": "utility",
+      "type": "overview",
+      "moduleName": "utility",
+      "shortDescription": "Utility",
+      "keywords": "barney barneyutility commons components file html include javascript js min module newton overview provide src text type usage utilities utility version"
+    },
+    {
+      "section": "version",
+      "id": "utility.BarneyUtility",
+      "shortName": "utility.BarneyUtility",
+      "type": "object",
+      "moduleName": "utility",
+      "shortDescription": "Use Utility service",
+      "keywords": "$scope add addqueryparams angular array array1 array2 arraydiff barneyutility browser brutal brutalredirect call callback catch changes clickandgo clicked clicks code component console controller css current dependency detect directive easily element elements event example function getcurrentquerystring hashbang hello homepagecontroller http included ios item javascript link list log media mediamatcher mediaquery method methods_addqueryparams methods_arraydiff methods_brutalredirect methods_clickandgo methods_getcurrentquerystring methods_mediamatcher mock module mql newparams newurl note object params queries query redirect remove removes renamed return returns second service string url user utility var version world"
+    },
+    {
+      "section": "version",
+      "id": "welcome",
+      "shortName": "welcome",
+      "type": "overview",
+      "moduleName": "welcome",
+      "shortDescription": "Welcome to Barney!",
+      "keywords": "analytics angular barney callbacky components config dependency dict file find folder full history html import include included infinite js library list logger meta min module newton overview remember script src storage text type utility version yourangularapp"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/welcome",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
