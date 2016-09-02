2ceedfc99fc8c65c3fe5b5b747219962f5cd307d
aureliome <aurelio.me@gmail.com>
2016-09-02 12:05:40 +0200

    Livehtml now execute script tag (with src field and with js code inside)

--- examples/pages/livehtml.js
+++ examples/pages/livehtml.js
@@ -4,7 +4,11 @@ angular.module('example').controller('LivehtmlCtrl',
      ['$scope', 
      function($scope) {
 
-        $scope.content = "<a ng-click='test()'>TEST ME</a>";
+        $scope.content = '<div>';
+        $scope.content += '<a ng-click="test()">TEST ME</a>';
+        $scope.content += '<script>console.log("it is inside (one)");</script>';
+        $scope.content += '<script src="//widgets.infostradasports.com/gns.widget.loader.js" data-widget_id="buongiorno-euro2016-schedule-it">console.log("it is inside (two)");</script>';
+        $scope.content += '</div>';
 
         $scope.test = function(){
             window.alert('TEST');
--- src/livehtml/live-html.dir.js
+++ src/livehtml/live-html.dir.js
@@ -9,10 +9,22 @@
  *
  * @example
  * <pre>
- *  // "UNSAFE_CODE": '<form action="go.php"><input type="text" name="name" /><iframe src="go.mp4" /></form>'
- *  
- *      <div live-html="UNSAFE_CODE"></div>
- *  // the directive prints '<form action="go.php"><input type="text" name="name" /><iframe src="go.mp4" /></form>'
+ *  <div live-html="UNSAFE_CODE"></div>
+ *
+ * <!--
+ *  if UNSAFE_CODE is 
+ *  '<form action="go.php"><input type="text" name="name" /><iframe src="go.mp4" /></form>'
+ *  then the directive prints 
+ *  '<form action="go.php"><input type="text" name="name" /><iframe src="go.mp4" /></form>'
+ *
+ *  if UNSAFE_CODE is 
+ *  '<script src="widget.js"></script>'
+ *  then the directive loads widget.js
+ *
+ *  if UNSAFE_CODE is 
+ *  '<script>window.alert('test');</script>'
+ *  then the directive executes javascript code inside <script> tag (window.alert)
+ * -->
  * </pre>
  */
 
@@ -25,15 +37,53 @@ angular.module('barney').directive('liveHtml',
             scope: {
                 liveHtml: '='
             },
-            link: function($scope, $element) {
+            controller: function($scope, $element) {
 
                 $scope.$watch('liveHtml', function(liveHtml) {
                     if(liveHtml){
                         $element.html(liveHtml);
                         $compile($element.contents())($scope.$parent);
                     }
-                    
                 });
+
+                this.liveScript = function(jsLink){
+                    var s = document.createElement('script');
+                    s.src = jsLink;
+                    document.body.appendChild(s);
+                };
+
+                this.liveJs = function(jsCode){
+                    Function(jsCode)();
+                };
+
+            }
+        };
+    }]
+);
+
+angular.module('barney').directive('script', 
+    [function() {
+
+        return {
+            restrict: 'E',
+            scope: false,
+            require: '?^^liveHtml',
+            link: function($scope, $element, $attrs, liveHtmlCtrl) {
+
+                // if <script> is child of live-html directive
+                if(liveHtmlCtrl){
+
+                    // if <script src="..."></script>
+                    if($element[0].src){
+                        liveHtmlCtrl.liveScript($element[0].src);
+                    }
+                    
+                    // if <script>...</script>
+                    if($element[0].innerHTML){
+                        liveHtmlCtrl.liveJs($element[0].innerHTML);
+                    }
+
+                }
 
             }
         };
