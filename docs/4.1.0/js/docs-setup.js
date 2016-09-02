008555d9faf2afb5c451ccdd6e99b2f9c4d0eb5f
aureliome <aurelio.me@gmail.com>
2016-09-02 12:30:31 +0200

    Version 4.1.0

---
+++ docs/4.1.0/js/docs-setup.js
@@ -0,0 +1,160 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "browser",
+      "shortName": "browser",
+      "type": "overview",
+      "moduleName": "browser",
+      "shortDescription": "Browser",
+      "keywords": "barney barneybrowser browser components file include js manage min module overview service src text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "browser.BarneyBrowser",
+      "shortName": "browser.BarneyBrowser",
+      "type": "object",
+      "moduleName": "browser",
+      "shortDescription": "To use Browser service, you have to add BarneyBrowser dependency to your component (i.e: directive, controller...):",
+      "keywords": "$scope add addqueryparams angular bar barneybrowser browser brutal brutalredirect call callback called calling catch change changes clickandgo clicked clicks component console controller current dependency directive dosomething event function getprevpath getprevstate getqueryparams goback hashbang hello homepagecontroller href html http init ios item javascript link log matches media mediamatcher mediaquery method methods mock module mql newparams news newurl ng-click object params path query redirect remember return returns saved service string trigger triggered url user version visited world"
+    },
+    {
+      "section": "version",
+      "id": "config",
+      "shortName": "config",
+      "type": "overview",
+      "moduleName": "config",
+      "shortDescription": "Config",
+      "keywords": "barney barneyconfig components config environment file filter include js json min module object overview provider reads src stored text type usage variables version"
+    },
+    {
+      "section": "version",
+      "id": "config.BarneyConfig",
+      "shortName": "config.BarneyConfig",
+      "type": "object",
+      "moduleName": "config",
+      "shortDescription": "To use Config service, you have to add BarneyConfig dependency to your component (directive, controller...):",
+      "keywords": "$scope add angular barneyconfig called calling component config configuration controller dependency enable_login false function homepagecontroller init key key_false key_true key_zero list method mock module newton_secret_key object options remember requires returns service specific true values version"
+    },
+    {
+      "section": "version",
+      "id": "config.filter:config",
+      "shortName": "config",
+      "type": "filter",
+      "moduleName": "config",
+      "shortDescription": "You can get a config value using the custom config filter",
+      "keywords": "barneyconfig config custom enable_login filter html init javascript prints version"
+    },
+    {
+      "section": "version",
+      "id": "dict",
+      "shortName": "dict",
+      "type": "overview",
+      "moduleName": "dict",
+      "shortDescription": "Dict",
+      "keywords": "barney barneydict components dict dictionary directive file filter include js min module overview provider src text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "dict.BarneyDict",
+      "shortName": "dict.BarneyDict",
+      "type": "object",
+      "moduleName": "dict",
+      "shortDescription": "To use Dict service, you have to add BarneyDict dependency to your component (directive, controller...):",
+      "keywords": "$scope add angular attributes barneydict called calling component controller default dependency dict dictionary existkey function hello homepagecontroller init key list method missing mock module notexistkey object options prints remember returns service showkey undefined valued version"
+    },
+    {
+      "section": "version",
+      "id": "dict.directive:dict",
+      "shortName": "dict",
+      "type": "directive",
+      "moduleName": "dict",
+      "shortDescription": "To use the directive instead of filter is useful if dictionary key contains html code.",
+      "keywords": "code dict dictionary directive filter hello html key prints valued_key version world"
+    },
+    {
+      "section": "version",
+      "id": "dict.filter:dict",
+      "shortName": "dict",
+      "type": "filter",
+      "moduleName": "dict",
+      "shortDescription": "You can get a dict value using the custom dict filter",
+      "keywords": "barneydict custom dict existkey filter hello html init javascript prints version"
+    },
+    {
+      "section": "version",
+      "id": "infinite",
+      "shortName": "infinite",
+      "type": "overview",
+      "moduleName": "infinite",
+      "shortDescription": "Infinite",
+      "keywords": "barney components directive feature file include infinite js min module overview scroll src text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "infinite.directive:InfiniteScroll",
+      "shortName": "InfiniteScroll",
+      "type": "directive",
+      "moduleName": "infinite",
+      "shortDescription": "Allow infinite scroll on your page.",
+      "keywords": "$scope activate add allow api argument attribute boolean bottom call callapimethod choose data directive disable disabled div element elements enable example executed flag function functiontocall html infinite infinite-callback infinite-enable infinite-offset infinite-scroll item javascript list method ng-repeat offset pass re-enable reached reaching reenable reenableinfinite response scroll true version wait"
+    },
+    {
+      "section": "version",
+      "id": "livehtml",
+      "shortName": "livehtml",
+      "type": "overview",
+      "moduleName": "livehtml",
+      "shortDescription": "Livehtml",
+      "keywords": "barney code components directive file html include js livehtml min module overview pre-controls print src template text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "livehtml.directive:livehtml",
+      "shortName": "livehtml",
+      "type": "directive",
+      "moduleName": "livehtml",
+      "shortDescription": "With live-html directive, you can print html code without any pre-controls.",
+      "keywords": "action alert code directive executes html inside javascript js live-html livehtml loads mp4 php pre-controls print prints src tag test text type unsafe_code version widget"
+    },
+    {
+      "section": "version",
+      "id": "meta",
+      "shortName": "meta",
+      "type": "overview",
+      "moduleName": "meta",
+      "shortDescription": "Meta",
+      "keywords": "barney barneymeta components dinamically file fill include js manage meta metatags min module overview service src tags text type usage version"
+    },
+    {
+      "section": "version",
+      "id": "meta.BarneyMeta",
+      "shortName": "meta.BarneyMeta",
+      "type": "object",
+      "moduleName": "meta",
+      "shortDescription": "To use Meta service, you have to add BarneyMeta dependency to your component (i.e: directive, controller...):",
+      "keywords": "$scope add angular automatically barneymeta call called calling change component controller current default defaults dependency description directive event function homepagecontroller html image init javascript jpg key list manually meta metatags method mock module ng-bind object og pass passing phase print recommended remember revert reverted route routechangestart service set single standard standard-image tags title values version zoom"
+    },
+    {
+      "section": "version",
+      "id": "welcome",
+      "shortName": "welcome",
+      "type": "overview",
+      "moduleName": "welcome",
+      "shortDescription": "Welcome to Barney!",
+      "keywords": "action angular app barney browser config dict discover divided include indipendent infinite instruction libraries livehtml meta module modules open overview performs purpose purposes read remember set version yourangularapp"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/welcome",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
